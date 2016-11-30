e7ad596816bc9ed6156f67432c799f5e1321eea6
D3ftsu <sahraouisharp@gmail.com>
2016-11-30 23:05:37 +0100

    Update BaseUlt2.lua

--- BaseUlt2/BaseUlt2.lua
+++ BaseUlt2/BaseUlt2.lua
@@ -12,8 +12,8 @@ function BaseUlt2:__init()
 	Callback.Add("Draw", function() self:Draw() end)
 	Callback.Add("ProcessRecall", function(unit, recall) self:ProcessRecall(unit, recall) end)
 	self.SupportedMaps, self.SupportedChampions = {CRYSTAL_SCAR, TWISTED_TREELINE, SUMMONERS_RIFT}, {"Ashe", "Draven", "Ezreal", "Jinx", "Karthus", "Lux", "Gangplank", "Ziggs"}
-    if not table.contains(self.SupportedMaps, Game.mapID) then print("BaseUlt2 - Map not supported!") return end
-    if not table.contains(self.SupportedChampions, myHero.charName) then print("BaseUlt2 - Champion not supported!") print("Current supported champions are : " ..table.concat(self.SupportedChampions, ", ")) return end
+    	if not table.contains(self.SupportedMaps, Game.mapID) then print("BaseUlt2 - Map not supported!") return end
+    	if not table.contains(self.SupportedChampions, myHero.charName) then print("BaseUlt2 - Champion not supported!") print("Current supported champions are : " ..table.concat(self.SupportedChampions, ", ")) return end
 	self:LoadBaseUltMenu()
 	Callback.Add("Tick", function() self:Tick() end)
 end
@@ -21,44 +21,44 @@ end
 function BaseUlt2:LoadData()
 	self.Sprite = Sprite("BaseUlt2/RecallTracker.png")
 	self.UltimateData = {
-    	["Ashe"] = {Delay = 0.25, Speed = 1600, Width = 130, Collision = true, Damage = function(source, target) return getdmg("R", target, source) end},
-    	["Draven"] = {Delay = 0.4, Speed = 2000, Width = 160, Collision = true, Damage = function(source, target) return getdmg("R", target, source) * 0.7 end},
-    	["Ezreal"] = {Delay = 1, Speed = 2000, Damage = function(source, target) return getdmg("R", target, source) * 0.7 end},
-    	["Jinx"] = {Delay = 0.6, Speed = 1700, Width = 140, Collision = true, Damage = function(source, target) return getdmg("R", target, source, 2) end},
-    	["Karthus"] = {Delay = 3.125, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
-    	["Lux"] = {Delay = 1, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
-    	["Gangplank"] = {Delay = 1, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
-    	["Ziggs"] = {Delay = 0, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
-    }
-    self.TimeToHit = 0
-	self.Allies, self.Enemies, self.RecallData, self.EnemyData = {}, {}, {}, {}
+    		["Ashe"] = {Delay = 0.25, Speed = 1600, Width = 130, Collision = true, Damage = function(source, target) return getdmg("R", target, source) end},
+    		["Draven"] = {Delay = 0.4, Speed = 2000, Width = 160, Collision = true, Damage = function(source, target) return getdmg("R", target, source) * 0.7 end},
+    		["Ezreal"] = {Delay = 1, Speed = 2000, Damage = function(source, target) return getdmg("R", target, source) * 0.7 end},
+    		["Jinx"] = {Delay = 0.6, Speed = 1700, Width = 140, Collision = true, Damage = function(source, target) return getdmg("R", target, source, 2) end},
+    		["Karthus"] = {Delay = 3.125, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
+    		["Lux"] = {Delay = 1, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
+    		["Gangplank"] = {Delay = 1, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
+    		["Ziggs"] = {Delay = 0, Speed = math.huge, Damage = function(source, target) return getdmg("R", target, source) end},
+    	}
+    	self.TimeToHit = 0
+	self.Allies, self.Enemies, self.RecallData, self.EnemyData, self.IncomingDamages = {}, {}, {}, {}, {}
 	for i = 1, Game.HeroCount() do
 	  	local unit = Game.Hero(i)
   	  	if unit.isMe then 
-  	    	goto continue
+  	    		goto continue
   	  	end
   	  	if unit.isAlly then 
-  	    	table.insert(self.Allies, unit)
+  	    		table.insert(self.Allies, unit)
   	  	else
-  	    	self.EnemyData[unit.networkID] = 0
-  	    	table.insert(self.Enemies, unit)
+  	    		self.EnemyData[unit.networkID] = 0
+  	    		table.insert(self.Enemies, unit)
   	  	end
   	  	::continue::
-    end
-    for i = 1, Game.ObjectCount() do
+    	end
+    	for i = 1, Game.ObjectCount() do
   	  	local object = Game.Object(i)
   	  	if object.isAlly or object.type ~= Obj_AI_SpawnPoint then 
-  	    	goto continue
+  	    		goto continue
   	  	end
   	  	self.EnemySpawnPos = object
   	  	break
   	  	::continue::
-    end
+    	end
 end
 
 function BaseUlt2:LoadBaseUltMenu()
 	self.Menu:MenuElement({type = MENU, id = "TeamUlt", name = "TeamUlt"})
-    for i, ally in pairs(self.Allies) do
+    	for i, ally in pairs(self.Allies) do
   		if table.contains(self.SupportedChampions, ally.charName) then
   			self.Menu.TeamUlt:MenuElement({id = ally.charName, name = "TeamUlt with "..ally.charName, value = false})
   		end
@@ -74,18 +74,18 @@ end
 
 function BaseUlt2:LoadRecallTrackerMenu()
 	self.Menu = MenuElement({type = MENU, id = "BaseUlt2", name = "BaseUlt2", leftIcon = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/a/a8/Super_Mega_Death_Rocket%21.png"})
-    self.Menu:MenuElement({type = MENU, id = "Tracker", name = "Recall Tracker"})
-    self.Menu.Tracker:MenuElement({id = "Enabled", name = "Show Recalls", value = true})
-    self.Menu.Tracker:MenuElement({id = "X", name = "X Offset", value = Game.Resolution().x - 260, min = 0, max = Game.Resolution().x - 260, step = 1, callback = function(value) self.Sprite:SetPos(value, self.Sprite.y) end})
-    self.Menu.Tracker:MenuElement({id = "Y", name = "Y Offset", value = Game.Resolution().y - 250, min = 0, max = Game.Resolution().y - 51, step = 1, callback = function(value) self.Sprite:SetPos(self.Sprite.x, value) end})
-    self.Sprite:SetPos(self.Menu.Tracker.X:Value(), self.Menu.Tracker.Y:Value())
+    	self.Menu:MenuElement({type = MENU, id = "Tracker", name = "Recall Tracker"})
+    	self.Menu.Tracker:MenuElement({id = "Enabled", name = "Show Recalls", value = true})
+    	self.Menu.Tracker:MenuElement({id = "X", name = "X Offset", value = Game.Resolution().x - 260, min = 0, max = Game.Resolution().x - 260, step = 1, callback = function(value) self.Sprite:SetPos(value, self.Sprite.y) end})
+    	self.Menu.Tracker:MenuElement({id = "Y", name = "Y Offset", value = Game.Resolution().y - 250, min = 0, max = Game.Resolution().y - 51, step = 1, callback = function(value) self.Sprite:SetPos(self.Sprite.x, value) end})
+    	self.Sprite:SetPos(self.Menu.Tracker.X:Value(), self.Menu.Tracker.Y:Value())
 end
 
 function BaseUlt2:ProcessRecall(unit, recall)
 	if not unit.isEnemy then return end
 	if recall.isStart then
-    	table.insert(self.RecallData, {object = unit, start = Game.Timer(), duration = (recall.totalTime*0.001)})
-    else
+    		table.insert(self.RecallData, {object = unit, start = Game.Timer(), duration = (recall.totalTime*0.001)})
+    	else
       	for i, rc in pairs(self.RecallData) do
         	if rc.object.networkID == unit.networkID then
           		table.remove(self.RecallData, i)
@@ -95,10 +95,10 @@ function BaseUlt2:ProcessRecall(unit, recall)
 end
 
 function BaseUlt2:GetRecallData(unit)
-    for i, recall in pairs(self.RecallData) do
-    	if recall.object.networkID == unit.networkID then
-    		return {isRecalling = true, timeToRecall = recall.start+recall.duration-Game.Timer()}
-	    end
+    	for i, recall in pairs(self.RecallData) do
+    		if recall.object.networkID == unit.networkID then
+    			return {isRecalling = true, timeToRecall = recall.start+recall.duration-Game.Timer()}
+	    	end
 	end
 	return {isRecalling = false, timeToRecall = 0}
 end
@@ -110,9 +110,9 @@ end
 function BaseUlt2:GetPredictedHealth(unit, time)
 	local shield = 0
 	if unit.charName == "Yasuo" then
-	    if unit.mana == unit.maxMana then
+	    	if unit.mana == unit.maxMana then
 			shield = ({100,105,110,115,120,130,140,150,165,180,200,225,255,290,330,380,440,510})[unit.levelData.lvl]
-	    end
+	    	end
 	elseif unit.charName == "Blitzcrank" then
 		if GotBuff(unit, "manabarriericon") > 0 then
 			shield = unit.visible and unit.mana or unit.mana+unit.mpRegen*(Game.Timer()-self.EnemyData[unit.networkID]+time) * 0.5
@@ -132,7 +132,7 @@ function BaseUlt2:GetTimeToReachBase(unit, data)
 	end
 	if unit.charName == "Jinx" then
 		missilespeed = distance > 1350 and (2295000 + (distance - 1350) * 2200) / distance or data.Speed
-    end
+    	end
 	return distance / missilespeed + delay
 end
 
@@ -141,8 +141,8 @@ function BaseUlt2:CanUseUlt(unit)
 end
 
 function BaseUlt2:ColorGradient(percent) 
-    local percent = math.min(99, percent)
-    return Draw.Color(255, percent < 50 and 255 or math.ceil(255 * ((50 - percent % 50) / 50)), percent >= 50 and 255 or math.ceil(255 * (percent / 50)), 0)
+    	local percent = math.min(99, percent)
+    	return Draw.Color(255, percent < 50 and 255 or math.ceil(255 * ((50 - percent % 50) / 50)), percent >= 50 and 255 or math.ceil(255 * (percent / 50)), 0)
 end
 
 function BaseUlt2:Draw()
@@ -151,7 +151,7 @@ function BaseUlt2:Draw()
 	for i, recall in pairs(self.RecallData) do
 		if Game.Timer() - recall.start < recall.duration then
 			Draw.Rect(self.Sprite.x+6, self.Sprite.y+13, (recall.start+recall.duration-Game.Timer()) / recall.duration * 238, 7, self:ColorGradient((recall.start+recall.duration-Game.Timer()) / recall.duration * 100))
-            Draw.Text(recall.object.charName.. " " ..math.ceil((1 - (Game.Timer() - recall.start) / recall.duration) * 100) .. "% (" .. math.ceil(self:GetPredictedHealth(recall.object, 0)) .. " HP)", 15, self.Sprite.x - 40 + ((recall.start+recall.duration-Game.Timer()) / recall.duration) * 238, self.Sprite.y - 10 - i * 15, Draw.Color(255, 255, 255, 255))
+            		Draw.Text(recall.object.charName.. " " ..math.ceil((1 - (Game.Timer() - recall.start) / recall.duration) * 100) .. "% (" .. math.ceil(self:GetPredictedHealth(recall.object, 0)) .. " HP)", 15, self.Sprite.x - 40 + ((recall.start+recall.duration-Game.Timer()) / recall.duration) * 238, self.Sprite.y - 10 - i * 15, Draw.Color(255, 255, 255, 255))
 			Draw.Line(self.Sprite.x+6+(recall.start+recall.duration-Game.Timer()) / recall.duration * 238, self.Sprite.y+13, self.Sprite.x+6+(recall.start+recall.duration-Game.Timer()) / recall.duration * 238, self.Sprite.y + 5 - i * 15, 2, Draw.Color(150,255,255,255))
 			if self.TimeToHit ~= 0 then
 				Draw.Text("|", 20, self.Sprite.x + 2 + self.TimeToHit/recall.duration * 238, self.Sprite.y+6, Draw.Color(255,255,0,0))
@@ -160,14 +160,22 @@ function BaseUlt2:Draw()
 	end
 end
 
+function BaseUlt2:GetTotalDamage()
+	local n = 0
+	for i, damage in pairs(self.IncomingDamages) do
+    		n = n + damage
+    	end
+    	return n
+end
+
 function BaseUlt2:Tick()
 	for i, enemy in pairs(self.Enemies) do
+		Draw.Text(tostring(getdmg("R", enemy, myHero) * 0.7), 20, enemy.pos2D.x, enemy.pos2D.y)
 		if enemy.visible then
 			self.EnemyData[enemy.networkID] = Game.Timer()
 		end
 	end
 	if not self.Menu.Enabled:Value() or myHero.dead or not self:CanUseUlt(myHero) or myHero.pos:DistanceTo(self.EnemySpawnPos.pos) > myHero:GetSpellData(3).range then return end
-	local damage = 0
 	for i, enemy in pairs(self.Enemies) do
 		if enemy.valid and not enemy.dead and not self.Menu.BlackList[enemy.charName]:Value() and self:GetRecallData(enemy).isRecalling then
 			for k, ally in pairs(self.Allies) do
@@ -175,31 +183,34 @@ function BaseUlt2:Tick()
 				if ally.pos:DistanceTo(self.EnemySpawnPos) > ally:GetSpellData(3).range then goto continue end
 				if ally.valid and not ally.dead and self.Menu.TeamUlt[ally.charName]:Value() and self:CanUseUlt(ally) then
 					if self.Menu.Collision:Value() and self:GetUltimateData(ally).Collision and self:IsColliding(ally.pos, enemy, self:GetUltimateData(ally).Width) then
+						self.IncomingDamages[ally.networkID] = 0
 						goto continue
 					end
-				end
-				local timeToHit = self:GetTimeToReachBase(ally, self:GetUltimateData(ally))
-				local timeToRecall = self:GetRecallData(enemy).timeToRecall
-                if timeToRecall >= timeToHit then
-            		damage = damage + self:GetUltimateData(ally).Damage(ally, enemy)
-                elseif timeToRecall < timeToHit then
-                    goto continue
-                end
+				    	local timeToHit = self:GetTimeToReachBase(ally, self:GetUltimateData(ally))
+			    		local timeToRecall = self:GetRecallData(enemy).timeToRecall
+               	 			if timeToRecall >= timeToHit then
+            					self.IncomingDamages[ally.networkID] = self:GetUltimateData(ally).Damage(ally, enemy)
+            				elseif timeToRecall < timeToHit - 0.125 then
+            					self.IncomingDamages[ally.networkID] = 0
+                			end
+                		end
 				::continue::
 			end
 			if self.Menu.Collision:Value() and self:GetUltimateData(myHero).Collision and self:IsColliding(myHero.pos, enemy, self:GetUltimateData(myHero).Width) then return end
 			local timeToHit = self:GetTimeToReachBase(myHero, self:GetUltimateData(myHero))
 			local timeToRecall = self:GetRecallData(enemy).timeToRecall
-            if timeToRecall >= timeToHit then
-            	damage = damage + self:GetUltimateData(myHero).Damage(myHero, enemy)
-            end
-            if damage < self:GetPredictedHealth(enemy, timeToRecall) then return end
-            self.TimeToHit = timeToHit
-            if timeToRecall - timeToHit > 0.1 or self.Menu.PanicKey:Value() then return end
-            Control.CastSpell("R", self.EnemySpawnPos.posMM.x, self.EnemySpawnPos.posMM.y) -- Minimap casting
-            self.TimeToHit = 0
-        end
-    end
+            		if timeToRecall >= timeToHit then
+            			self.IncomingDamages[myHero.networkID] = self:GetUltimateData(myHero).Damage(myHero, enemy)
+            		elseif timeToRecall < timeToHit then
+            			self.IncomingDamages[myHero.networkID] = 0
+            		end
+            		if self:GetTotalDamage() < self:GetPredictedHealth(enemy, timeToRecall) then return end
+            		self.TimeToHit = timeToHit
+            		if timeToRecall - timeToHit > 0.1 or self.Menu.PanicKey:Value() then return end
+            		Control.CastSpell("R", self.EnemySpawnPos.posMM.x, self.EnemySpawnPos.posMM.y) -- Minimap casting
+            		self.TimeToHit = 0
+        	end
+    	end
 end
 
 function BaseUlt2:ProjectVectorOnSegment(v1, v2, v)
@@ -213,17 +224,17 @@ function BaseUlt2:ProjectVectorOnSegment(v1, v2, v)
 end
 
 function BaseUlt2:IsColliding(from, unit, width)
-  for i, enemy in pairs(self.Enemies) do
-    if enemy.valid and not enemy.dead and unit.networkID ~= enemy.networkID then
-      local ProjectionInfo = self:ProjectVectorOnSegment(from, self.EnemySpawnPos.pos, enemy.pos)
-      if ProjectionInfo.IsOnSegment and ProjectionInfo.PointSegment:DistanceTo(enemy.pos) < width+enemy.boundingRadius and from:DistanceTo(self.EnemySpawnPos.pos) > from:DistanceTo(enemy.pos) then
-        return true
-      end
-    end
-  end
-  return false
+	for i, enemy in pairs(self.Enemies) do
+    		if enemy.valid and not enemy.dead and unit.networkID ~= enemy.networkID then
+      			local ProjectionInfo = self:ProjectVectorOnSegment(from, self.EnemySpawnPos.pos, enemy.pos)
+      			if ProjectionInfo.IsOnSegment and ProjectionInfo.PointSegment:DistanceTo(enemy.pos) < width+enemy.boundingRadius and from:DistanceTo(self.EnemySpawnPos.pos) > from:DistanceTo(enemy.pos) then
+        			return true
+      			end
+    		end
+  	end
+  	return false
 end
 
 function OnLoad()
-  BaseUlt2()
+  	BaseUlt2()
 end
